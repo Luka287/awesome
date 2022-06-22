4987b6b33f085919dbe4b1cd5c9b53aa35287476
Erik Dubois <erik.dubois@gmail.com>
2022-06-22 18:23:55 +0400

    Removed useless files

--- rc.lua
+++ rc.lua
@@ -119,7 +119,7 @@ mymainmenu = awful.menu({ items = { { "Awesome", myawesomemenu, beautiful.awesom
 
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     menu = mymainmenu })
+                                     menu = mymainmenu})
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
